16d0d61df8b744d041c83fbeffea0e9123157e59
kusumachalasani <kchalasa@redhat.com>
2023-04-24 11:12:37 +0530

    Add different experiments
    Signed-off-by: kusumachalasani <kchalasa@redhat.com>

--- remote_monitoring_demo/demo.py
+++ remote_monitoring_demo/demo.py
@@ -15,10 +15,12 @@ limitations under the License.
 """
 
 from kruize.kruize import *
+from helpers.utils import *
 import sys, getopt
 import json
 import os
 import time
+import csv
 
 def generate_json(find_arr, json_file, filename, i):
 
@@ -66,8 +68,41 @@ def main(argv):
     perf_profile_json_file = "./json_files/resource_optimization_openshift.json"
     create_performance_profile(perf_profile_json_file)
 
+    recommendations_json_arr = []
+    ## Create experiments from the experiments_list and use related csv data to updateResults
+    experiments_list = ['eap-app_deploymentconfig_america', 'example_replicationcontroller_america', 'tfb-qrh_deployment_tfb-tests', 'rhsso-operator_deployment_sso']
+    for exp in experiments_list:
+        experiment_json = "./json_files/experiment_jsons/" + exp + ".json"
+        experiment_csv = "./csv_data/" + exp + ".csv"
+        create_experiment(experiment_json)
+        json_data = json.load(open(experiment_json))
+        experiment_name = json_data[0]['experiment_name']
+
+        with open(experiment_csv, 'r') as csv_file:
+            reader = csv.reader(csv_file)
+            header = next(reader)
+            for row in reader:
+                #if not any(row):
+                #    continue
+                with open('intermediate.csv', mode='w', newline='') as outfile:
+                    writer = csv.writer(outfile)
+                    writer.writerow(header)
+                    writer.writerow(row)
+                    
+                # Convert the results csv to json
+                resultsjson_file = "./json_files/experiment_jsons/results.json"
+                create_json_from_csv("./intermediate.csv",resultsjson_file)
+                update_results(resultsjson_file)
+                
+                # Sleep
+                #time.sleep(1)
+                
+                reco = list_recommendations(experiment_name)
+                recommendations_json_arr.append(reco)
+
+
     # Create experiments using the specified json
-    num_exps = 10
+    num_exps = 1
     for i in range(num_exps):
         tmp_create_exp_json_file = "/tmp/create_exp.json"
         generate_json(find, create_exp_json_file, tmp_create_exp_json_file, i)
@@ -85,7 +120,6 @@ def main(argv):
             print("Namespace = ", namespace)
 
     # Post the experiment results
-    recommendations_json_arr = []
     num_exp_res = 151
 
     print("\n*************************************************************************************")
@@ -101,6 +135,7 @@ def main(argv):
 
         reco = list_recommendations(experiment_name)
         recommendations_json_arr.append(reco)
+
 
     # Dump the results & recommendations into json files
     with open('recommendations_data.json', 'w') as f:
---
+++ remote_monitoring_demo/helpers/utils.py
@@ -0,0 +1,155 @@
+import csv
+import json
+import os
+import datetime
+import sys
+import getopt
+
+# Convert any date format to kruize specific format
+def convert_date_format(input_date_str):
+    DATE_FORMATS = ["%a %b %d %H:%M:%S %Z %Y", "%Y-%m-%dT%H:%M:%S.%f", "%a %b %d %H:%M:%S UTC %Y", "%Y-%m-%d %H:%M:%S %Z", "%Y-%m-%d %H:%M:%S %z %Z"]
+
+    for date_format in DATE_FORMATS:
+        try:
+            dt = datetime.datetime.strptime(input_date_str, date_format)
+            dt_utc = dt.astimezone(datetime.timezone.utc)
+            output_date_str = dt_utc.strftime("%Y-%m-%dT%H:%M:%S.000Z")
+            return output_date_str
+        except ValueError:
+            continue
+    raise ValueError(f"Unrecognized date format: {input_date_str} ")
+
+# Convert the csv to json
+def create_json_from_csv(csv_file_path, outputjsonfile):
+    json_data = []
+    deployments = []
+    mebibyte = 1048576
+
+    with open(csv_file_path, 'r') as csvfile:
+        csvreader = csv.DictReader(csvfile)
+
+        for row in csvreader:
+            container_metrics_all = {}
+            container_metrics = []
+
+            if row["cpu_request_container_avg"]:
+                container_metrics.append({
+			"name": "cpuRequest",
+                        "results": {
+                            "aggregation_info": {
+                                "sum": float(row["cpu_request_container_sum"]),
+                                "avg": float(row["cpu_request_container_avg"]),
+                                "units": "cores"
+                                }
+                            }
+			})
+            if row["cpu_limit_container_avg"]:
+                container_metrics.append({
+			"name" : "cpuLimit",
+                        "results": {
+                            "aggregation_info": {
+                                "sum": float(row["cpu_limit_container_sum"]),
+                                "avg": float(row["cpu_limit_container_avg"]),
+                                "units": "cores"
+                                }
+                            }
+                        })
+            if row["cpu_throttle_container_max"]:
+                container_metrics.append({
+			"name" : "cpuThrottle",
+                        "results": {
+                            "aggregation_info": {
+                                "sum": float(row["cpu_throttle_container_sum"]),
+                                "max": float(row["cpu_throttle_container_max"]),
+                                "avg": float(row["cpu_throttle_container_avg"]),
+                                "units": "cores"
+                                }
+                            }
+                        })
+            container_metrics.append({
+		    "name" : "cpuUsage",
+                    "results": {
+                        "aggregation_info": {
+                            "sum": float(row["cpu_usage_container_sum"]),
+                            "min": float(row["cpu_usage_container_min"]),
+                            "max": float(row["cpu_usage_container_max"]),
+                            "avg": float(row["cpu_usage_container_avg"]),
+                            "units": "cores"
+                            }
+                        }
+                    })
+            if row["memory_request_container_avg"]:
+                container_metrics.append({
+			"name" : "memoryRequest",
+                        "results": {
+                            "aggregation_info": {
+                                "sum": float(row["memory_request_container_sum"])/mebibyte,
+                                "avg": float(row["memory_request_container_avg"])/mebibyte,
+                                "units": "MiB"
+                                }
+                            }
+                        })
+            if row["memory_limit_container_avg"]:
+                container_metrics.append({
+			"name" : "memoryLimit",
+                        "results": {
+                            "aggregation_info": {
+                                "sum": float(row["memory_limit_container_sum"])/mebibyte,
+                                "avg": float(row["memory_limit_container_avg"])/mebibyte,
+                                "units": "MiB"
+                                }
+                            }
+                        })
+            container_metrics.append({
+		    "name" : "memoryUsage",
+                    "results": {
+                        "aggregation_info": {
+                            "min": float(row["memory_usage_container_min"])/mebibyte,
+                            "max": float(row["memory_usage_container_max"])/mebibyte,
+                            "sum": float(row["memory_usage_container_sum"])/mebibyte,
+                            "avg": float(row["memory_usage_container_avg"])/mebibyte,
+                            "units": "MiB"
+                        }
+                    }
+                })
+            container_metrics.append({
+		    "name" : "memoryRSS",
+                    "results": {
+                        "aggregation_info": {
+                            "min": float(row["memory_rss_usage_container_min"])/mebibyte,
+                            "max": float(row["memory_rss_usage_container_max"])/mebibyte,
+                            "sum": float(row["memory_rss_usage_container_sum"])/mebibyte,
+                            "avg": float(row["memory_rss_usage_container_avg"])/mebibyte,
+                            "units": "MiB"
+                        }
+                    }
+                })
+
+            container = {
+                "container_image_name": row["image_name"],
+                "container_name": row["container_name"],
+                "metrics": container_metrics
+            }
+
+            containers = [container]
+            kubernetes_object = {
+                "type": row["k8_object_type"],
+                "name": row["k8_object_name"],
+                "namespace": row["namespace"],
+                "containers": containers
+            }
+            kubernetes_objects = [kubernetes_object]
+            experiment = {
+                "version": "1.0",
+                "experiment_name": row["k8_object_name"] + '|' + row["k8_object_type"] + '|' + row["namespace"],
+                "interval_start_time": convert_date_format(row["interval_start"]),
+                "interval_end_time": convert_date_format(row["interval_end"]),
+                "kubernetes_objects": kubernetes_objects
+            }
+            json_data.append(experiment)
+
+    with open(outputjsonfile, "w") as json_file:
+        json.dump(json_data, json_file)
+
+
+#create_json_from_csv('../csv_data/rhsso-operator_deployment_sso.csv', 'finaldata.csv')
