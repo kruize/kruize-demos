902863942262c0a85c89f9ce0daaf1bd2da48860
msvinaykumar <vinakuma@redhat.com>
2023-08-22 18:41:37 +0530

    fixed conflicts
    Signed-off-by: msvinaykumar <vinakuma@redhat.com>

--- remote_monitoring_demo/demo.py
+++ remote_monitoring_demo/demo.py
@@ -14,17 +14,20 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from kruize.kruize import *
-from helpers.utils import create_json_from_csv
-import sys, getopt
-import json
-import os
-import time
+import copy
 import csv
+import getopt
 import itertools
+import json
+import sys
+import time
+from datetime import datetime
+
+from helpers.utils import create_json_from_csv
+from kruize.kruize import *
+
 
 def generate_json(find_arr, json_file, filename, i):
-
     with open(json_file, 'r') as file:
         data = file.read()
 
@@ -35,10 +38,12 @@ def generate_json(find_arr, json_file, filename, i):
     with open(filename, 'w') as file:
         file.write(data)
 
+
 def main(argv):
     cluster_type = "minikube"
     create_exp_json_file = "./json_files/create_exp.json"
     find = []
+    num_entries = 97
 
     json_data = json.load(open(create_exp_json_file))
 
@@ -49,7 +54,7 @@ def main(argv):
     print(find)
 
     try:
-        opts, args = getopt.getopt(argv,"h:c:b:")
+        opts, args = getopt.getopt(argv, "h:c:d:")
     except getopt.GetoptError:
         print("demo.py -c <cluster type>")
         sys.exit(2)
@@ -59,8 +64,11 @@ def main(argv):
             sys.exit()
         elif opt == '-c':
             cluster_type = arg
+        elif opt == '-d' and arg is not None:
+            num_entries = int(arg) * 96
+            num_entries += 1
 
-    print("demo.py -c %s"%(cluster_type))
+    print("demo.py -c %s" % (cluster_type))
 
     # Form the kruize url
     form_kruize_url(cluster_type)
@@ -68,6 +76,38 @@ def main(argv):
     # Create the performance profile
     perf_profile_json_file = "./json_files/resource_optimization_openshift.json"
     create_performance_profile(perf_profile_json_file)
+
+    recommendations_json_arr = []
+    ## Create experiments from the experiments_list and use related csv data to updateResults
+    experiments_list = ['eap-app_deploymentconfig_america', 'example_replicationcontroller_america',
+                        'tfb-qrh_deployment_tfb-tests', 'rhsso-operator_deployment_sso']
+    for exp in experiments_list:
+        experiment_json = "./json_files/experiment_jsons/" + exp + ".json"
+        experiment_csv = "./csv_data/" + exp + ".csv"
+        create_experiment(experiment_json)
+        json_data = json.load(open(experiment_json))
+        experiment_name = json_data[0]['experiment_name']
+
+        with open(experiment_csv, 'r') as csv_file:
+            reader = csv.reader(csv_file)
+            header = next(reader)
+
+            limited_reader = itertools.islice(reader, num_entries)
+            for row in limited_reader:
+                if not any(row):
+                    continue
+                with open('intermediate.csv', mode='w', newline='') as outfile:
+                    writer = csv.writer(outfile)
+                    writer.writerow(header)
+                    writer.writerow(row)
+
+                # Convert the results csv to json
+                resultsjson_file = "./json_files/experiment_jsons/results.json"
+                create_json_from_csv("./intermediate.csv", resultsjson_file)
+                update_results(resultsjson_file)
+
+                reco = list_recommendations(experiment_name)
+                recommendations_json_arr.append(reco)
 
     # Create experiments using the specified json
     num_exps = 1
@@ -88,7 +128,6 @@ def main(argv):
             print("Namespace = ", namespace)
 
     # Post the experiment results
-    recommendations_json_arr = []
     num_exp_res = 151
 
     print("\n*************************************************************************************")
@@ -112,66 +151,18 @@ def main(argv):
         # Get the current batch
         batch = bulk_payload[current_index:current_index + batch_size]
 
-        file_path = './resource_usage_metrics_data/result_%s_to_%s.json'%(current_index,batch_size)
+        file_path = './resource_usage_metrics_data/result_%s_to_%s.json' % (current_index, batch_size)
         with open(file_path, 'w') as json_file:
             json.dump(batch, json_file)
         update_results(file_path)
-
+        batch_deep_copy = copy.deepcopy(batch)
+        for item in batch_deep_copy:
+            item['interval_start_time'] = datetime.strptime(item['interval_start_time'], "%Y-%m-%dT%H:%M:%S.%fZ")
+            item['interval_end_time'] = datetime.strptime(item['interval_end_time'], "%Y-%m-%dT%H:%M:%S.%fZ")
+        max_time = max(batch_deep_copy, key=lambda x: x['interval_end_time'])['interval_end_time']
+        update_recommendations(experiment_name, max_time.strftime("%Y-%m-%dT%H:%M:%S.%fZ")[:-4] + "Z")
         # Update the current index for the next batch
         current_index += batch_size
-    end_dates = [
-        "2023-04-02T00:45:00.000Z",
-        "2023-04-02T01:15:00.421Z",
-        "2023-04-02T01:30:00.433Z",
-        "2023-04-02T01:45:00.000Z",
-        "2023-04-02T02:00:00.000Z",
-        "2023-04-02T02:15:00.000Z",
-        "2023-04-02T02:30:00.000Z",
-        "2023-04-02T02:45:00.000Z",
-        "2023-04-02T03:00:00.000Z",
-        "2023-04-02T03:15:00.000Z",
-        "2023-04-02T03:45:00.000Z",
-        "2023-04-02T04:00:00.000Z",
-        "2023-04-02T04:15:00.000Z",
-        "2023-04-02T04:30:00.000Z",
-        "2023-04-02T04:45:00.000Z",
-        "2023-04-02T05:00:00.000Z",
-        "2023-04-02T05:15:00.000Z",
-        "2023-04-02T05:30:00.000Z",
-        "2023-04-02T05:45:00.000Z",
-        "2023-04-02T06:00:00.000Z",
-        "2023-04-02T06:15:00.000Z",
-        "2023-04-02T06:30:00.000Z",
-        "2023-04-02T07:00:00.000Z",
-        "2023-04-02T07:15:00.000Z",
-        "2023-04-02T07:30:00.000Z",
-        "2023-04-02T07:45:00.000Z",
-        "2023-04-02T08:00:00.000Z",
-        "2023-04-02T08:15:00.000Z",
-        "2023-04-02T08:30:00.000Z",
-        "2023-04-02T08:45:00.000Z",
-        "2023-04-02T09:00:00.000Z",
-        "2023-04-02T09:15:00.000Z",
-        "2023-04-02T09:30:00.000Z",
-        "2023-04-02T09:45:00.000Z",
-        "2023-04-02T10:00:00.000Z",
-        "2023-04-02T10:15:00.000Z",
-        "2023-04-02T10:30:00.000Z",
-        "2023-04-02T10:45:00.000Z",
-        "2023-04-02T11:00:00.770Z",
-        "2023-04-02T11:15:00.770Z",
-        "2023-04-02T11:30:00.770Z",
-        "2023-04-02T11:45:00.770Z",
-        "2023-04-02T12:00:00.770Z",
-        "2023-04-02T12:15:00.770Z",
-        "2023-04-02T12:30:00.770Z",
-        "2023-04-02T12:45:00.770Z",
-        "2023-04-02T13:00:00.110Z",
-        "2023-04-02T13:15:00.350Z",
-        "2023-04-02T13:30:00.680Z"
-    ]
-    for enddate in end_dates:
-        update_recommendations(experiment_name,enddate)
     # Sleep
     time.sleep(1)
     reco = list_recommendations(experiment_name)
--- remote_monitoring_demo/remote_monitoring_demo.sh
+++ remote_monitoring_demo/remote_monitoring_demo.sh
@@ -33,13 +33,14 @@ visualize=0
 PYTHON_CMD=python3
 
 function usage() {
-	echo "Usage: $0 [-s|-t] [-o kruize-image] [-r] [-c cluster-type] [-d] [--visualize]"
+	echo "Usage: $0 [-s|-t] [-o kruize-image] [-r] [-c cluster-type] [-d] [--days=] [--visualize]"
 	echo "s = start (default), t = terminate"
 	echo "r = restart kruize monitoring only"
 	echo "o = kruize image. Default - docker.io/kruize/autotune_operator:<version as in pom.xml>"
 	echo "c = supports minikube and openshift cluster-type"
 	echo "d = duration of benchmark warmup/measurement cycles"
 	echo "p = expose prometheus port"
+	echo "days = number of days data to push into kruize. Do not exceed 15."
 	echo "visualize = Visualize the resource usage and recommendations in grafana (Yet to be implemented)"
 	exit 1
 }
@@ -72,7 +73,7 @@ function kruize_install() {
 	echo "6. Installing Kruize"
 	if [ ! -d autotune ]; then
 		echo "ERROR: autotune dir not found."
-		if [ ${autotune_restart} -eq 1 ]; then
+		if [[ ${autotune_restart} -eq 1 ]]; then
 			echo "ERROR: Kruize not running. Wrong use of restart command"
 		fi
 		exit -1
@@ -122,8 +123,8 @@ function kruize_install() {
 }
 
 function remote_monitoring_experiments() {
-	echo "Running demo.py..."
-	"${PYTHON_CMD}" demo.py -c "${CLUSTER_TYPE}"
+	echo "Running demo.py with ${DATA_DAYS} day data..."
+  "${PYTHON_CMD}" demo.py -c "${CLUSTER_TYPE}" -d "${DATA_DAYS}"
 }
 
 function pronosana_backfill() {
@@ -332,8 +333,10 @@ monitoring_restart=0
 start_demo=1
 EXPERIMENT_START=0
 terminate=0
+# Default no.of data entries to an experiment
+DATA_DAYS=1
 # Iterate through the commandline options
-while getopts o:c:d:prsbt-: gopts
+while getopts o:c:d:prstu-: gopts
 do
 
 	 case ${gopts} in
@@ -379,7 +382,6 @@ do
 			usage
 	esac
 done
-
 
 if [ ${start_demo} -eq 1 ]; then
 	remote_monitoring_demo_start
