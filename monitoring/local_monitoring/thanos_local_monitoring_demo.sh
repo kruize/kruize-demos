f830328a563607c62a2404acb1a9d56e5bde8009
Chandrakala Subramanyam <csubrama@redhat.com>
2024-07-30 17:36:59 +0530

    Adding a timeout for thanos route
    Signed-off-by: Chandrakala Subramanyam <csubrama@redhat.com>

--- monitoring/local_monitoring/thanos_local_monitoring_demo.sh
+++ monitoring/local_monitoring/thanos_local_monitoring_demo.sh
@@ -134,7 +134,9 @@ function get_urls() {
 
 		${kubectl_cmd} expose service kruize
 		${kubectl_cmd} expose service kruize-ui-nginx-service
-		${kubectl_cmd} annotate route kruize --overwrite haproxy.router.openshift.io/timeout=60s
+		${kubectl_cmd} annotate route kruize --overwrite haproxy.router.openshift.io/timeout=120s
+
+		oc -n thanos-bench annotate route thanos-query-frontend --overwrite haproxy.router.openshift.io/timeout=300s
 
 		export KRUIZE_URL=$(${kubectl_cmd} get route kruize --no-headers -o wide -o=custom-columns=NODE:.spec.host)
 		export KRUIZE_UI_URL=$(${kubectl_cmd} get route kruize-ui-nginx-service --no-headers -o wide -o=custom-columns=NODE:.spec.host)
