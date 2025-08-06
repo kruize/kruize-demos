97438ad6eb31d69d1303c9d5e33d198aab1ffa96
Nicholas Caughey <ncaughey@li-8906324c-2c4e-11b2-a85c-c72a7795f096.ibm.com>
2025-08-06 10:41:09 +0100

    update readme and the clean up script

--- monitoring/local_monitoring/cleanup_openshift.sh
+++ monitoring/local_monitoring/cleanup_openshift.sh
@@ -9,3 +9,10 @@ oc delete deployment kruize -n openshift-tuning
 oc delete deployment kruize-db -n openshift-tuning
 
 
+# Delete existing resources to force recreation
+oc delete clusterrole kruize-recommendation-updater kruize-monitoring-access 2>/dev/null || true
+oc delete clusterrolebinding kruize-monitoring-view kruize-recommendation-updater-crb kruize-prometheus-reader kruize-system-reader kruize-monitoring-access-crb 2>/dev/null || true
+
+# Delete ConfigMap to trigger recreation
+oc delete configmap kruizeconfig -n openshift-tuning
+oc delete deployment kruize -n openshift-tuning
