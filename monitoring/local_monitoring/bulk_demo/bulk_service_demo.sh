chandrams/autotune-demo

Included bulk demo for thanos datasource
Signed-off-by: Chandrakala Subramanyam <csubrama@redhat.com>

monitoring/local_monitoring/bulk_demo/bulk_service_demo.sh
@@ -32,6 +32,8 @@ export target="crc"
 
 KIND_IP=127.0.0.1
 KRUIZE_PORT=8080
+THANOS_PORT=9090
+MINIO_PORT=9000
 KRUIZE_UI_PORT=8081
 TECHEMPOWER_PORT=8082
 
@@ -39,13 +41,16 @@ PYTHON_CMD=python3
 export LOG_FILE="${current_dir}/kruize-bulk-demo.log"
 
 function usage() {
-	echo "Usage: $0 [-s|-t] [-c cluster-type] [-l] [-p] [-r] [-i kruize-image] [-u kruize-ui-image]"
+	echo "Usage: $0 [-s|-t] [-c cluster-type] [-l] [-p] [-f] [-i kruize-image] [-u kruize-ui-image]"
+	echo "[-z] [-q thanos datasource url. Default - http://${KIND_IP}:${THANOS_PORT}]"
 	echo "c = supports minikube, kind and openshift cluster-type"
 	echo "i = kruize image. Default - quay.io/kruize/autotune_operator:<version as in pom.xml>"
 	echo "p = expose prometheus port"
 	echo "l = to deploy TFB with load"
-	echo "r = restart kruize only"
+	echo "f = enable environment setup"
 	echo "s = start (default), t = terminate"
+	echo "z = register thanos datasource with Kruize (supported only with openshift/kind cluster type)"
+	echo "q = thanos datasource url. Default - http://${KIND_IP}:${THANOS_PORT}]"
 	echo "u = Kruize UI Image. Default - quay.io/kruize/kruize-ui:<version as in package.json>"
 	echo "n = namespace of benchmark. Default - default"
 	echo "d = duration to run the benchmark load"
@@ -55,7 +60,7 @@ function usage() {
 
 function kruize_bulk() {
   echo "Running bulk_demo.py..." >> "${LOG_FILE}" 2>&1
-  "${PYTHON_CMD}" -u bulk_demo.py -c "${CLUSTER_TYPE}"
+  "${PYTHON_CMD}" -u bulk_demo.py -c "${CLUSTER_TYPE}" -z "${thanos}"
   {
   echo
   echo "######################################################"
@@ -72,11 +77,14 @@ export KRUIZE_DOCKER_IMAGE=""
 export prometheus=0
 export env_setup=0
 export start_demo=1
+export thanos=0
+export ds_url="http://thanos-query-frontend-example-query.thanos-operator-system.svc:9090"
+export minio_url="http://${KIND_IP}:${MINIO_PORT}"
 export APP_NAMESPACE="default"
 export LOAD_DURATION="1200"
 
 # Iterate through the commandline options
-while getopts c:i:n:d:lprstu: gopts
+while getopts c:i:n:d:q:lpfstzu: gopts
 do
 	case "${gopts}" in
 		c)
@@ -97,8 +105,14 @@ do
 		s)
 			start_demo=1
 			;;
+		z)
+			thanos=1
+			;;
 		t)
 			start_demo=0
+			;;
+		q)
+			ds_url="${OPTARG}"
 			;;
 		u)
 			KRUIZE_UI_DOCKER_IMAGE="${OPTARG}"
@@ -115,6 +129,12 @@ do
 done
 
 export demo="bulk"
+if [[ "${CLUSTER_TYPE}" == "minikube" && "${thanos}" == "1" ]]; then
+	echo "Thanos Demo is not supported on minikube cluster"
+	usage
+	exit 1
+fi
+
 if [ ${start_demo} -eq 1 ]; then
 	echo > "${LOG_FILE}" 2>&1
 	kruize_local_demo_setup

kruize/kruize-demos

monitoring/local_monitoring/bulk_demo/bulk_service_demo.sh
@@ -35,6 +35,8 @@ export target="crc"
 
 KIND_IP=127.0.0.1
 KRUIZE_PORT=8080
+THANOS_PORT=9090
+MINIO_PORT=9000
 KRUIZE_UI_PORT=8081
 TECHEMPOWER_PORT=8082
 KRUIZE_OPERATOR=0
@@ -44,13 +46,16 @@ PYTHON_CMD=python3
 export LOG_FILE="${current_dir}/kruize-bulk-demo.log"
 
 function usage() {
-	echo "Usage: $0 [-s|-t] [-c cluster-type] [-l] [-p] [-r] [-i kruize-image] [-u kruize-ui-image]"
+	echo "Usage: $0 [-s|-t] [-c cluster-type] [-l] [-p] [-f] [-i kruize-image] [-u kruize-ui-image]"
+	echo "[-z] [-q thanos datasource url. Default - http://${KIND_IP}:${THANOS_PORT}]"
 	echo "c = supports minikube, kind and openshift cluster-type"
 	echo "i = kruize image. Default - quay.io/kruize/autotune_operator:<version as in pom.xml>"
 	echo "p = expose prometheus port"
 	echo "l = to deploy TFB with load"
-	echo "r = restart kruize only"
+	echo "f = enable environment setup"
 	echo "s = start (default), t = terminate"
+	echo "z = register thanos datasource with Kruize (supported only with openshift/kind cluster type)"
+	echo "q = thanos datasource url. Default - http://${KIND_IP}:${THANOS_PORT}]"
 	echo "u = Kruize UI Image. Default - quay.io/kruize/kruize-ui:<version as in package.json>"
 	echo "n = namespace of benchmark. Default - default"
 	echo "d = duration to run the benchmark load"
@@ -63,7 +68,7 @@ function usage() {
 
 function kruize_bulk() {
   echo "Running bulk_demo.py..." >> "${LOG_FILE}" 2>&1
-  "${PYTHON_CMD}" -u bulk_demo.py -c "${CLUSTER_TYPE}"
+  "${PYTHON_CMD}" -u bulk_demo.py -c "${CLUSTER_TYPE}" -z "${thanos}"
   {
   echo
   echo "######################################################"
@@ -78,11 +83,14 @@ export KRUIZE_DOCKER_IMAGE=""
 export prometheus=0
 export env_setup=0
 export start_demo=1
+export thanos=0
+export ds_url="http://thanos-query-frontend-example-query.thanos-operator-system.svc:9090"
+export minio_url="http://${KIND_IP}:${MINIO_PORT}"
 export APP_NAMESPACE="default"
 export LOAD_DURATION="1200"
 
 # Iterate through the commandline options
-while getopts c:i:n:d:klfprstu:o: gopts
+while getopts c:i:n:d:q:klpfstzu: gopts
 do
 	case "${gopts}" in
 		c)
@@ -103,8 +111,14 @@ do
 		s)
 			start_demo=1
 			;;
+		z)
+			thanos=1
+			;;
 		t)
 			start_demo=0
+			;;
+		q)
+			ds_url="${OPTARG}"
 			;;
 		u)
 			KRUIZE_UI_DOCKER_IMAGE="${OPTARG}"
@@ -134,6 +148,11 @@ if [[ "${CLUSTER_TYPE}" == "minikube" ]] || [[ "${CLUSTER_TYPE}" == "kind" ]]; t
   KRUIZE_OPERATOR=0
 else
   NAMESPACE="openshift-tuning"
+
+if [[ "${CLUSTER_TYPE}" == "minikube" && "${thanos}" == "1" ]]; then
+	echo "Thanos Demo is not supported on minikube cluster"
+	usage
+	exit 1
 fi
 
 if [ ${start_demo} -eq 1 ]; then