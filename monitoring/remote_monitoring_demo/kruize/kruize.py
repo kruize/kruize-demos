3b9a5e8deffe34533b59b6df9bce2baf95f7cec1
kusuma chalasani <kchalasa@redhat.com>
2026-01-16 18:19:15 +0530

    use shell-free subprocess call for safety
    Signed-off-by: kusuma chalasani <kchalasa@redhat.com>

--- monitoring/remote_monitoring_demo/kruize/kruize.py
+++ monitoring/remote_monitoring_demo/kruize/kruize.py
@@ -58,25 +58,37 @@ def form_kruize_url(cluster_type):
     global KRUIZE_UI_URL
     if (cluster_type == "minikube"):
         port = subprocess.run(
-            ['kubectl -n monitoring get svc kruize --no-headers -o=custom-columns=PORT:.spec.ports[*].nodePort'],
-            shell=True, stdout=subprocess.PIPE)
+            [
+                "kubectl",
+                "-n", "monitoring",
+                "get", "svc", "kruize",
+                "--no-headers",
+                "-o=custom-columns=PORT:.spec.ports[*].nodePort"
+            ],
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True, text=True)
 
-        AUTOTUNE_PORT = port.stdout.decode('utf-8').strip('\n')
+        AUTOTUNE_PORT = port.stdout.strip()
 
-        ui_port = subprocess.run([
-                                     'kubectl -n monitoring get svc kruize-ui-nginx-service --no-headers -o=custom-columns=PORT:.spec.ports[*].nodePort'],
-                                 shell=True, stdout=subprocess.PIPE)
+        ui_port = subprocess.run(
+                [
+                    "kubectl",
+                    "-n", "monitoring",
+                    "get", "svc", "kruize-ui-nginx-service",
+                    "--no-headers",
+                    "-o=custom-columns=PORT:.spec.ports[*].nodePort"
+                ],
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True, text=True)
 
-        KRUIZE_UI_PORT = ui_port.stdout.decode('utf-8').strip('\n')
+        KRUIZE_UI_PORT = ui_port.stdout.strip()
 
-        ip = subprocess.run(['minikube ip'], shell=True, stdout=subprocess.PIPE)
-        SERVER_IP = ip.stdout.decode('utf-8').strip('\n')
+        ip = subprocess.run(["minikube", "ip"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True, text=True)
+        SERVER_IP = ip.stdout.strip()
         URL = "http://" + str(SERVER_IP) + ":" + str(AUTOTUNE_PORT)
         KRUIZE_UI_URL = "http://" + str(SERVER_IP) + ":" + str(KRUIZE_UI_PORT)
 
     elif (cluster_type == "openshift"):
 
-        subprocess.run(['oc expose svc/kruize -n openshift-tuning'], shell=True, stdout=subprocess.PIPE)
+        subprocess.run(["oc", "expose", "svc/kruize", "-n", "openshift-tuning"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True, text=True)
         ip = subprocess.run(
             [
                 'oc status -n openshift-tuning | grep "kruize" | grep -v "kruize-ui" | grep -v "kruize-db" | grep port | cut -d " " -f1 | cut -d "/" -f3'],
@@ -86,7 +98,7 @@ def form_kruize_url(cluster_type):
         print("IP = ", SERVER_IP)
         URL = "http://" + str(SERVER_IP)
 
-        subprocess.run(['oc expose svc/kruize-ui-nginx-service -n openshift-tuning'], shell=True, stdout=subprocess.PIPE)
+        subprocess.run(["oc", "expose", "svc/kruize-ui-nginx-service", "-n", "openshift-tuning"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True, text=True)
         ip = subprocess.run(
             [
                 'oc status -n openshift-tuning | grep "kruize-ui-nginx-service" | grep port | cut -d " " -f1 | cut -d "/" -f3'],
