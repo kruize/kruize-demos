3a2f5913b171970086cc138483b75bc080769782
Shreya Biradar <shbirada@ibm.com>
2026-04-24 16:57:45 +0530

    Add flag option -r for specifying the Kruize operator branch
    Signed-off-by: Shreya Biradar <shbirada@ibm.com>

--- monitoring/local_monitoring/local_monitoring_demo.sh
+++ monitoring/local_monitoring/local_monitoring_demo.sh
@@ -25,6 +25,9 @@ source ${current_dir}/common.sh
 # Default operator docker image repo
 KRUIZE_OPERATOR_DOCKER_REPO="quay.io/kruize/kruize-operator"
 
+# Default operator branch
+export KRUIZE_OPERATOR_BRANCH="main"
+
 # Default docker image repo
 export KRUIZE_DOCKER_REPO="quay.io/kruize/autotune_operator"
 
@@ -48,6 +51,7 @@ function usage() {
 	echo "i = kruize image. Default - quay.io/kruize/autotune_operator:<version as in pom.xml>"
 	echo "u = Kruize UI Image. Default - quay.io/kruize/kruize-ui:<version as in package.json>"
 	echo "o = Specify custom Kruize operator image: -o <image>. Default - quay.io/kruize/kruize-operator:<version as in Makefile>"
+	echo "r = Specify Kruize operator branch to clone: -r <branch>. Default - main"
 	echo "e = supports container, namespace and gpu"
 	echo "b = deploy the benchmark."
 	echo "m = manifests of the benchmark"
@@ -75,7 +79,7 @@ export BENCHMARK_MANIFESTS="resource_provisioning_manifests"
 export EXPERIMENT_TYPE=""
 export KRUIZE_OPERATOR_IMAGE=""
 # Iterate through the commandline options
-while getopts bc:d:e:fi:klm:no:pstu: gopts
+while getopts bc:d:e:fi:klm:no:psr:tu: gopts
 do
 	case "${gopts}" in
 		b)
@@ -125,6 +129,9 @@ do
 	 	k)
       			KRUIZE_OPERATOR=0
       			;;
+		r)
+			export KRUIZE_OPERATOR_BRANCH="${OPTARG}"
+			;;
 		*)
 			usage
 	esac
